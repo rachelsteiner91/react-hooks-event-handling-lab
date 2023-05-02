3aaad45959d1963248d8d51a81e4fd3c5970b0b1
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-02 12:26:35 -0400

    recat hooks event handling lab commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
+function handleChange(){ //here is the name of a teacher and that eamer is going to teach me all these things, it's just the namee of something that lays out the instructions
+    console.log('Entering password...')}
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type='password' onChange={handleChange}></input>
+        </div>
     )
 }
 
